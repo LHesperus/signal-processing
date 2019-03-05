093175143a42af2246c68c41194e73b5493dde2a
LHesperus <2639350497@qq.com>
2019-03-05 21:20:23 +0800

    Update GMSK.m

--- Modulated signal/GMSK/GMSK.m
+++ Modulated signal/GMSK/GMSK.m
@@ -19,6 +19,7 @@ xc=cos(2*pi*fc*t);
 xs=sin(2*pi*fc*t);
 
 %% Gaussian Filter
+%BTs=100000;
 BTs=0.3;
 B=BTs/Ts;
 
@@ -30,25 +31,33 @@ B=BTs/Ts;
 %I couldn't do it with h convolution, and then I used qunc function.
 g_t=qfunc(2*pi*B*(t-Ts/2-4.5*Ts)/sqrt(log(2)))-qfunc(2*pi*B*(t+Ts/2-4.5*Ts)/sqrt(log(2)));%4,5Ts is used to display all g_t
 figure
-plot(t,g_t)
-
+plot(t/Ts,g_t)
+title('Rectangular impulse response of Gauss filter')
+legend('BT=0.3')
+xlabel('t/Tb')
+ylabel('g(t)')
+grid on
 
 %% gengerate bit sequence
 a=2*(rand(1,N)>0.5)-1;
-%a=[1,1,-1,1,-1,-1,1,1,-1,1] %%test signal
+a=[1,1,-1,1,-1,-1,1,1,-1,1] %%test signal
 %a=[1,1,1,-1,-1,1,1,1,-1,-1,-1,-1,-1,-1,-1]
-a=[1,1,1,-1,-1,1,1,1,-1,-1]
+%a=[1,1,1,-1,-1,1,1,1,-1,-1]
 a_sample=repmat(a,r,1);
 a_sample=a_sample(:)';
 
 figure
-plot(a_sample)
+plot(t/Ts,a_sample)
+xlabel('t/Tb')
+ylabel('a(t)')
+grid on
 %% conv
 a_fil=conv(a_sample,g_t)/r;%I can't explain it here./r,It seems right.
 a_fil=a_fil(round(4.5*Ts/T):end);%Make up for the delays(4.5Ts) previously used
 figure
-plot(a_fil)
-title('a_fil')
+plot((0:length(a_fil)-1)*T/Ts,a_fil)
+title('filtered signal')
+xlabel('t/Tb')
 
 
 %% additive phase
@@ -57,12 +66,24 @@ for ii=1:length(t)
 end
 figure
 %Although the trend is right, the peak value is getting smaller and I don't know what the problem is.
-plot(theta)
+plot(t/Ts,theta/pi);
+title('additive phase  of GMSK')
+xlabel('t/Tb')
+ylabel('/\pi')
 
 theta1=wrapToPi(theta);
 figure
-plot(theta1)
+
+plot(t/Ts,theta1/pi);
+title('additive phase  of GMSK [0,2\pi]')
+xlabel('t/Tb')
+ylabel('/\pi')
 %% GMSK
 s=cos(theta).*xc-sin(theta).*xs;
 figure
-plot(s)
+plot(t/Ts,s)
+title('GMSK')
+xlabel('t/Tb')
+%% Spectral
+figure
+plotSpectral(s,fs);
