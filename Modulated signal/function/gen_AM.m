dd551fda5047fa503aba074e77864ab51b2f531f
LHesperus <2639350497@qq.com>
2019-05-11 15:08:37 +0800

    -

---
+++ Modulated signal/function/gen_AM.m
@@ -0,0 +1,25 @@
+%Generating AM signal
+function y_AM=gen_AM(A,fc,fs,L,m_a)
+%use orthogonal modulation generate modulated siganl
+%You can see the original signal v in the code.
+if nargin==0
+    A=1;                                                    %%Ampltitude
+    fc=1e4;                                                 %carrier frequency
+    fs=1e5;                                                 %sample frequency
+    L=10000;                                                %length of signal
+    m_a=0.3;                                                %modulation index,|m_a|<1
+
+end
+T=1/fs;                                                 %sample time
+t=(0:L-1)*T;                                            %time vector
+if nargin==0
+    v=A*cos(2*pi*1000*t);                                   %modulation signal
+end
+xs=sin(2*pi*fc*t);
+xc=cos(2*pi*fc*t);
+                                               
+I_AM=A+m_a*v;
+Q_AM=0;
+y_AM=I_AM.*xc+Q_AM.*xs;
+
+
---
+++ Modulated signal/function/gen_ASK2.m
@@ -0,0 +1,35 @@
+%Generating 2ASK signal
+function y_2ask=gen_ASK2(A,fc,fs,Rs,L)
+%use orthogonal modulation generate modulated siganl
+%You can see the original signal v in the code.
+if nargin==0
+    A=1;                                                    %%Ampltitude
+    fc=1e4;                                                 %carrier frequency
+    fs=1e5;                                                 %sample frequency
+    Rs=1e3;                                                 %symbol rate of digital signal
+    L=10000;                                                %length of signal
+end
+T=1/fs;                                                     %sample time
+t=(0:L-1)*T;                                                %time vector
+xs=sin(2*pi*fc*t);
+xc=cos(2*pi*fc*t);
+
+%2ASK
+%           _________          ______
+%   signal |         |        |      |      
+%----------|         |________|      |  so L/fs/T_ask is the length of a_n
+%           <-T_ask->                       
+%<-----------L points --------------->
+T_ask=1/Rs;                                          
+a_n=round(rand(1,round(L/(T_ask*fs))));
+a_ask=repmat(a_n,T_ask*fs,1);
+a_ask=a_ask(:)';
+%stairs(a_ask)                                        %figure 2ask,unuse plot
+I_ask=0;
+Q_ask=a_ask;
+y_2ask=I_ask.*xc+Q_ask.*xs;
+y_2ask=A*y_2ask;
+end
+
+
+
