f564855a80004e663d89d7e7ebac953d772ced90
LHesperus <2639350497@qq.com>
2019-05-14 22:08:17 +0800

    --

--- Modulated signal/AM.m
+++ Modulated signal/AM.m
@@ -7,7 +7,7 @@ close all;
 fc=1e4;                                                 %carrier frequency
 fs=1e5;                                                 %sample frequency
 T=1/fs;                                                 %sample time
-L=1000;                                                 %length of signal
+L=10000;                                                 %length of signal
 t=(0:L-1)*T;                                            %time vector
 A=1;                                                    %%Ampltitude
 v=A*cos(2*pi*1000*t);                                   %modulation signal
@@ -33,10 +33,42 @@ figure(3)
 plot(y_AM)
 title('m_a=0.5');
 
-m_a=1;                                            
+m_a=0.7;                                            
 I_AM=A+m_a*v;
 Q_AM=0;
 y_AM=I_AM.*xc+Q_AM.*xs;
 figure(4)
 plot(y_AM)
 title('m_a=1');
+
+ff=(-L/2:L/2-1)*(fs/L);
+f_offset=0.001*fc
+[I,Q]=DDC_filter(fs,fc+f_offset,4000,10,y_AM);
+figure
+subplot(2,1,1)
+plot(I)
+subplot(2,1,2)
+plot(Q)
+IQ=I+Q*j;
+figure
+subplot(2,1,1)
+plot(abs(IQ))
+subplot(2,1,2)
+plot(angle(IQ)./pi*180)
+figure
+plot(ff,abs(fftshift(fft(IQ))))
+IQ_adjust=IQ.*exp(-j*2*pi*f_offset*t);
+hold on
+plot(ff,abs(fftshift(fft(IQ_adjust))))
+legend('IQ','IQ_adjust')
+figure
+subplot(2,1,1)
+plot(real(IQ_adjust))
+subplot(2,1,2)
+plot(imag(IQ_adjust))
+
+figure
+subplot(2,1,1)
+plot(abs(IQ_adjust))
+subplot(2,1,2)
+plot(angle(IQ_adjust)./pi*180)
--- Modulated signal/ASK2.m
+++ Modulated signal/ASK2.m
@@ -6,9 +6,9 @@ close all;
 %You can see the original signal v in the code.
 fc=1e4;                                                 %carrier frequency
 fs=1e5;                                                 %sample frequency
-Rs=2e3;                                                 %symbol rate of digital signal
+Rs=1e3;                                                 %symbol rate of digital signal
 T=1/fs;                                                 %sample time
-L=1000;                                                  %length of signal
+L=10000;                                                  %length of signal
 t=(0:L-1)*T;                                            %time vector
 A=1;                                                    %%Ampltitude
 v=A*cos(2*pi*1000*t);                                   %modulation signal
@@ -30,6 +30,46 @@ I_ask=0;
 Q_ask=a_ask;
 y_2ask=I_ask.*xc+Q_ask.*xs;
 figure(1)
-plot(y_2ask)
+plot(y_2ask);title('y_ask')
 figure(2)
 plotSpectral(y_2ask,fs)
+
+y_2ask=awgn(y_2ask,10,'measured');
+
+ff=(-L/2:L/2-1)*(fs/L);
+f_offset=0.001*fc
+[I,Q]=DDC_filter(fs,fc+f_offset,4000,15,y_2ask);
+figure
+subplot(2,1,1)
+plot(I);title('DDC I')
+subplot(2,1,2)
+plot(Q);title('DDC Q')
+IQ=I+Q*j;
+figure
+subplot(2,1,1)
+plot(abs(IQ));title('abs(IQ)')
+subplot(2,1,2)
+plot(angle(IQ)./pi*180);title('angle DDC IQ')
+figure
+Y=abs(fftshift(fft(IQ)));
+plot(ff,Y);title('spectrum of DDC IQ')
+
+%%
+% 
+IQ_adjust=IQ.*exp(j*2*pi*f_offset*t);
+hold on
+Y_adjust=abs(fftshift(fft(IQ_adjust)));
+plot(ff,Y_adjust);title('Y_adjust')
+f_adjust=ff(find(Y_adjust==max(Y_adjust)))
+legend('IQ','IQ_adjust')
+figure
+subplot(2,1,1)
+plot(real(IQ_adjust));title('I Y_adjust')
+subplot(2,1,2)
+plot(imag(IQ_adjust));title('Q Y_adjust')
+
+figure
+subplot(2,1,1)
+plot(abs(IQ_adjust));title('abs IQ adjust')
+subplot(2,1,2)
+plot(angle(IQ_adjust)./pi*180);title('ansgle IQ adjust')
