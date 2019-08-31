b645669645aaa07e524a5ba452b86c25443a2fed
LHesperus <2639350497@qq.com>
2019-08-31 20:05:18 +0800

    -

--- Modulated signal/raised_cosine/gen_rcos/main.m
+++ Modulated signal/raised_cosine/gen_rcos/main.m
@@ -10,18 +10,16 @@ alpha=0.5;
 L=256*6;
 t=(-L/2:L/2-1)/fs;
 t(L/2+1)=1e-9;
-pi=3.1415926;
 %% root raised cosine
-h1=(sin((1-alpha)*pi*t/Tb)+4*alpha*t/Tb.*cos((1+alpha)*pi*t/Tb))./(pi*t/Tb.*(1-(4*alpha*t/Tb).^2));
+h_den=(pi*t/Tb.*(1-(4*alpha*t/Tb).^2));
+h1=(sin((1-alpha)*pi*t/Tb)+4*alpha*t/Tb.*cos((1+alpha)*pi*t/Tb))./h_den;
+for ii=1:length(h1)
+    if(h_den(ii)==0)
+        h1(ii)=(h1(ii-1)+h1(ii+1))/2;
+    end
+end
 h1=h1/(2*pi);% 2pi是我随便取的，貌似可以让升余弦峰值为1
 
-xxx=ones(1,100)+2*ones(1,100)*j;
-xxxx=zeros(1,4000);
-xxxx(1:40:end)=xxx;
-aaa=conv(h1,(xxxx)).';
-aaa=aaa(1:64:end);
-IQ_out=sym_synch_Gardner(aaa);
-aaaaaaaa
 h_rcos=conv(h1,h1);
 H1=fftshift(fft(h1));
 H_rcos=fftshift(fft(h_rcos));
@@ -36,7 +34,13 @@ subplot(2,2,4)
 plot(abs(H_rcos));title('H rcos')
 
 %% raised cosine
-h=sin(pi*t/Tb).*cos(pi*alpha*t/Tb)./(pi*t/Tb.*(1-(2*alpha*t/Tb).^2));
+h_den=(pi*t/Tb.*(1-(2*alpha*t/Tb).^2));
+h=sin(pi*t/Tb).*cos(pi*alpha*t/Tb)./h_den;
+for ii=1:length(h)
+    if(h_den(ii)==0)
+        h(ii)=(h(ii-1)+h(ii+1))/2;
+    end
+end
 figure
 subplot(2,1,1)
 plot(h)
@@ -46,7 +50,13 @@ plot(abs(fftshift(fft(h))))
 % compare
 figure
 for  alpha=0.2:0.2:0.8
-    h=sin(pi*t/Tb).*cos(pi*alpha*t/Tb)./(pi*t/Tb.*(1-(2*alpha*t/Tb).^2));
+    h_den=(pi*t/Tb.*(1-(2*alpha*t/Tb).^2));
+    h=sin(pi*t/Tb).*cos(pi*alpha*t/Tb)./h_den;
+    for ii=1:length(h)
+    if(h_den(ii)==0)
+        h(ii)=(h(ii-1)+h(ii+1))/2;
+    end
+end
     plot(h)
     hold on
 end
@@ -54,7 +64,13 @@ hold off
 legend('alpha=0.2','alpha=0.4','alpha=0.6','alpha=0.8')
 figure
 for  alpha=0.2:0.2:0.8
-    h=sin(pi*t/Tb).*cos(pi*alpha*t/Tb)./(pi*t/Tb.*(1-(2*alpha*t/Tb).^2));
+    h_den=(pi*t/Tb.*(1-(2*alpha*t/Tb).^2));
+    h=sin(pi*t/Tb).*cos(pi*alpha*t/Tb)./h_den;
+    for ii=1:length(h)
+        if(h_den(ii)==0)
+          h(ii)=(h(ii-1)+h(ii+1))/2;
+        end
+    end
     H=abs(fftshift(fft(h)));
     plot(H)
     hold on
@@ -64,7 +80,7 @@ legend('alpha=0.2','alpha=0.4','alpha=0.6','alpha=0.8')
 
 %% matlab func
 rf = 0.6;
-span = 60;
+span = 6;
 sps = 64 ;
 h1 = rcosdesign(rf,span,sps,'sqrt');
 h_rcos=conv(h1,h1);
