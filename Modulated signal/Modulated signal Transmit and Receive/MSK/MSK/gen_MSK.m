62b6d54983a89082382e273ad44d703155a0a76f
LHesperus <2639350497@qq.com>
2020-01-02 17:27:37 +0800

    --

--- Modulated signal/Modulated signal Transmit and Receive/MSK/MSK/gen_MSK.m
+++
@@ -1,7 +0,0 @@
-function [outputArg1,outputArg2] = gen_MSK(inputArg1,inputArg2)
-%GEN_MSK 此处显示有关此函数的摘要
-%   此处显示详细说明
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-
--- Modulated signal/Modulated signal Transmit and Receive/readme.m
+++
@@ -1,9 +0,0 @@
-%in data file
-Baseband=-1;
-IF=-2;
-IF2Base=-3;
-
-AM=1;
-LSB=2;
-USB=3;
-FM=4;
